1333eae7e3cfa5acbdf5dcb3856c2d87d0b87dbc
Konstantin Grushetsky <github@grushetsky.net>
2019-01-30 20:17:19 +0300

    Switch to Yarn as default package manager, upgrade dependencies
    Add Yarn lock files.

--- benchmarks/run.js
+++ benchmarks/run.js
@@ -13,13 +13,13 @@ try {
 	var npmPackage = require('./node_modules/classnames/package.json');
 } catch (e) {
 	log('There was an error loading the benchmark classnames package.\n' +
-		'Please make sure you have run `npm install` in ./benchmarks\n');
+		'Please make sure you have run `yarn install` in ./benchmarks\n');
 	process.exit(0);
 }
 
 if (localPackage.version !== npmPackage.version) {
 	log('Your local version (' + localPackage.version + ') does not match the installed version (' + npmPackage.version + ')\n\n' +
-		'Please run `npm update` in ./benchmarks to ensure you are benchmarking\n' +
+		'Please run `yarn upgrade` in ./benchmarks to ensure you are benchmarking\n' +
 		'the latest version of this package.\n');
 	process.exit(0);
 }
